a7f93385f785ce8fdf49d74ffa2df799615a64df
Leonardo de Figueiredo <38779415+leothesouthafrican@users.noreply.github.com>
2023-09-11 16:17:27 +0100

    add symptom grouping and autoencoder

--- Leo/autoencoder.py
+++ Leo/autoencoder.py
@@ -1,21 +1,31 @@
-from torch import nn
 import torch
+import torch.nn as nn
+import torch.optim as optim
 
 class Autoencoder(nn.Module):
-    def __init__(self, n_features, n_latent):
+    def __init__(self, input_dim, latent_dim):
         super(Autoencoder, self).__init__()
+        
+        self.latent_dim = latent_dim  # Add this line
+        
+        # Encoder
         self.encoder = nn.Sequential(
-            nn.Linear(n_features, 128),
+            nn.Linear(input_dim, 128),
             nn.ReLU(),
-            nn.Linear(128, n_latent)
-        )
-        self.decoder = nn.Sequential(
-            nn.Linear(n_latent, 128),
-            nn.ReLU(),
-            nn.Linear(128, n_features)
+            nn.Linear(128, latent_dim),
+            nn.ReLU()
         )
         
+        # Decoder
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, input_dim),
+            nn.Sigmoid()  # Sigmoid activation for binary output allowing us to use BCE loss
+        )
+
     def forward(self, x):
         x = self.encoder(x)
         x = self.decoder(x)
         return x
+
