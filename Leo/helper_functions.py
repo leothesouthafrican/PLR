c5ef3462395025b3eb3a191b1c29b0ff2e05d4eb
Leonardo de Figueiredo <38779415+leothesouthafrican@users.noreply.github.com>
2023-09-16 14:06:10 +0100

    add demographic data

--- Leo/helper_functions.py
+++ Leo/helper_functions.py
@@ -1,3 +1,56 @@
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+import pandas as pd
+import numpy as np
+
+# Used to compute the phi coefficient for the contingency table
+def phi_coefficient(contingency_table):
+    if contingency_table.shape[0] < 2 or contingency_table.shape[1] < 2:
+        return 0
+    a = contingency_table.iloc[0, 0]
+    b = contingency_table.iloc[0, 1]
+    c = contingency_table.iloc[1, 0]
+    d = contingency_table.iloc[1, 1]
+    numerator = (a * d) - (b * c)
+    denominator = np.sqrt((a + b) * (a + c) * (b + d) * (c + d))
+    return numerator / denominator if denominator != 0 else 0
+
+# Used to one-hot encode categorical attributes
+def one_hot_encode(df):
+    object_cols = df.select_dtypes(include=['object']).columns
+    encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+    encoded_attrs = encoder.fit_transform(df[object_cols])
+    
+    print("List of one-hot encoded columns:")
+    print(list(encoder.get_feature_names_out(object_cols)))
+
+    if len(object_cols) == encoder.n_features_in_:
+        encoded_df = pd.DataFrame(encoded_attrs, columns=encoder.get_feature_names_out(object_cols))
+    else:
+        encoded_df = pd.DataFrame(encoded_attrs, columns=encoder.get_feature_names_out())
+
+    return pd.concat([df.drop(object_cols, axis=1), encoded_df], axis=1)
+
+
+# Used to standardize numeric attributes
+# Used to standardize numeric attributes
+def standardize_numeric(df):
+    if df.empty:
+        print("DataFrame is empty, skipping standardization.")
+        return df
+    numeric_cols = df.select_dtypes(include=['float64', 'int64']).columns
+    if len(numeric_cols) == 0:
+        print("No numeric columns to standardize.")
+        return df
+
+    scaler = StandardScaler()
+    df[numeric_cols] = scaler.fit_transform(df[numeric_cols])
+
+    # Print the list of standardized columns
+    print("List of standardized columns:")
+    print(list(numeric_cols))
+    
+    return df
+
 # Function to print text within a box
 def print_boxed_text(lines, is_title=None):
     if not lines:
