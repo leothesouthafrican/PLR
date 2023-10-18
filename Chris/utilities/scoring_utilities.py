1bb385861bde39068ffe6016eecadfe6f1a6fafc
rusty-chris <rustydataltd@gmail.com>
2023-10-18 06:18:24 +0100

    changed fail return values to numerical for dbcv and silhouette - other cannot filt GP model for bayes search

--- Chris/utilities/scoring_utilities.py
+++ Chris/utilities/scoring_utilities.py
@@ -2,6 +2,12 @@ import hdbscan
 import numpy as np
 from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score
 
+fail_return_dict = {
+    'dbcv': -1,
+    'silhouette': -1,
+    'calinski_harabasz': np.nan,
+    'davies_bouldin': np.nan
+}
 
 def dbcv(data, labels, metric='euclidean'):
     try:
@@ -11,7 +17,7 @@ def dbcv(data, labels, metric='euclidean'):
         )
     except ValueError:
         print("ValueError caught in dbcv, returning nan.")
-        return np.nan
+        return fail_return_dict['dbcv']
 
 
 def dbcv_minkowski(data, labels):
@@ -19,19 +25,19 @@ def dbcv_minkowski(data, labels):
         return dbcv(data, labels, metric='minkowski')
     except ValueError:
         print("ValueError caught in dbcv_minkowski, returning nan.")
-        return np.nan
+        return fail_return_dict['dbcv']
 
 def silhouette(data, labels):
     try:
         num_labels = len(set(labels))
         if num_labels == 1:
             print("Warning: Valid number of clusters must be 2 or more.")
-            return np.nan
+            return fail_return_dict['silhouette']
         else:
             return silhouette_score(data, labels)
     except ValueError:
         print("ValueError caught in silhouette, returning nan.")
-        return np.nan
+        return fail_return_dict['silhouette']
 
 
 def calinski_harabasz(data, labels):
@@ -39,12 +45,12 @@ def calinski_harabasz(data, labels):
         num_labels = len(set(labels))
         if num_labels == 1:
             print("Warning: Valid number of clusters must be 2 or more.")
-            return np.nan
+            return fail_return_dict['calinski_harabasz']
         else:
             return calinski_harabasz_score(data, labels)
     except ValueError:
         print("ValueError caught in calinski_harabasz, returning nan.")
-        return np.nan
+        return fail_return_dict['calinski_harabasz']
 
 
 def davies_bouldin(data, labels):
@@ -55,9 +61,9 @@ def davies_bouldin(data, labels):
         num_labels = len(set(labels))
         if num_labels == 1:
             print("Warning: Valid number of clusters must be 2 or more.")
-            return np.nan
+            return fail_return_dict['davies_bouldin']
         else:
             return davies_bouldin_score(data, labels)
     except ValueError:
         print("ValueError caught in davies_bouldin, returning nan.")
-        return np.nan
+        return fail_return_dict['davies_bouldin']
