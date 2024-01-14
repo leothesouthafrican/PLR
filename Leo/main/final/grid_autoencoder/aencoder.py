3fa52cbb9bb575ce690e981a520f5b5836f472c8
Leonardo de Figueiredo <38779415+leothesouthafrican@users.noreply.github.com>
2024-01-14 14:15:17 +0100

    add saved weights

--- Leo/main/final/grid_autoencoder/aencoder.py
+++ Leo/main/final/grid_autoencoder/aencoder.py
@@ -4,14 +4,6 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 
-# Setting seeds for reproducibility
-torch.manual_seed(42)
-np.random.seed(42)
-if torch.cuda.is_available():
-    torch.cuda.manual_seed_all(42)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
-
 class Autoencoder(nn.Module):
     def __init__(self, input_size, hidden_size, latent_dim):
         super(Autoencoder, self).__init__()
@@ -35,7 +27,10 @@ class Autoencoder(nn.Module):
         x = self.decoder(x)
         return x
 
-def train_autoencoder(model, data, learning_rate, epochs):
+def train_autoencoder(model, data, learning_rate, epochs, random_state=42):
+
+    torch.manual_seed(random_state)
+
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     train_loader = DataLoader(TensorDataset(data, data), batch_size=32, shuffle=False)
@@ -50,3 +45,9 @@ def train_autoencoder(model, data, learning_rate, epochs):
             optimizer.step()
 
     return model
+
+def get_latent_representation(model, data):
+    model.eval()
+    with torch.no_grad():
+        latent_representation = model.encoder(data)
+    return latent_representation
